be3f0ace1e1ba6fc82b990accc50610dd54d4b9e
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-04-10 11:19:16 +0530

    Added Scripts

--- src/runMatlabBuild.ts
+++ src/runMatlabBuild.ts
@@ -65,6 +65,7 @@ export async function runCommand(hs: HelperScript, platform: string, architectur
     if (args) {
        execArgs = execArgs.concat(args);
     }
+    const exitCode;
     await core.group("Runnig tasks", async () => {
      const exitCode = await fn(rmcPath, execArgs);
      })
