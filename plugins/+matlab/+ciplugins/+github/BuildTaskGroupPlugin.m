59ffe83e82cffd4527b89a4309a726a16ff2da1a
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-04-10 15:01:40 +0530

    Added Scripts

--- plugins/+matlab/+ciplugins/+github/BuildTaskGroupPlugin.m
+++ plugins/+matlab/+ciplugins/+github/BuildTaskGroupPlugin.m
@@ -9,11 +9,11 @@ classdef BuildTaskGroupPlugin < matlab.buildtool.plugins.BuildRunnerPlugin
         if pluginData.TaskResults.Name == "error"
            disp("::error::" + pluginData.TaskResults.Name );
         else
-           disp("::info::\u001b[35m" + pluginData.TaskResults.Name );
+           disp("::group::" + pluginData.TaskResults.Name );
         end
             runTask@matlab.buildtool.plugins.BuildRunnerPlugin(plugin, pluginData);
             if pluginData.TaskResults.Name ~= "error"
-               %disp("::endgroup::");
+               disp("::endgroup::");
             end
         end
     end
