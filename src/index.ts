31c47e66bddf416641e1133be7e71839d35e5b46
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-04-23 12:31:32 +0530

    updated

--- src/index.ts
+++ src/index.ts
@@ -41,9 +41,9 @@ async function run() {
 
    // Adding Summary details in log
    core.info("\u001b[35m####### MATLAB build Summary #######");
-   core.info("\u001b[38mTasks run: "+ 3);
+   core.info("\u001b[32mTasks run: "+ 3);
    core.info("\u001b[38;2;255;0;0mTasks failed: "+ 0);
-   core.info("\u001b[43mTasks skipped: "+ 0);
+   core.info("\u001b[33mTasks skipped: "+ 0);
 
    //Addding summary Page
    core.summary
