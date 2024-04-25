dda56177af2603e579cea0e0676108ccbdced1ed
Nikhil Bhoski <47204011+nbhoski@users.noreply.github.com>
2024-04-25 15:15:42 +0530

    updated

--- src/index.ts
+++ src/index.ts
@@ -53,7 +53,7 @@ async function run() {
        [{data: 'Task Name', header: true}, {data: 'Status', header: true}, {data: 'Description', header: true}, {data: 'Duration (HH:MM:SS)', header: true}],
        ['build', 'Pass ✅', 'Builds the code', '00:00:01'],
        ['test', 'Fail ❌', 'Run tests', '00:00:05'],
-       ['verify', 'Skipped 🚫', 'Runs static analysis', '00:00:00']
+       ['deploy', 'Skipped 🚫', 'deploys code', '00:00:00']
      ])
      .addLink('View detailed build result', 'https://github.com')
      .write()
